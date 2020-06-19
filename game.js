76dba0eb816b73e1e7b3661be701a7ad0d10f279
bartoginski <ogitoziomal@gmail.com>
2020-06-19 22:07:57 +0200

    plus and minus changed, increment js added

--- game.js
+++ game.js
@@ -1,1 +1,33 @@
-console.log("test");
+// consts DOM
+
+const roundHeader = document.querySelector(".round-header");
+const targetNumber = document.querySelector(".targer-num");
+const addButton = document.querySelector(".add");
+const substrButton = document.querySelector(".substr");
+
+//scores
+const userScore = document.querySelector(".user-score");
+const computerScore = document.querySelector(".computer-score");
+
+//buttons
+const guessButton = document.querySelector(".make-guess");
+const roundButton = document.querySelector(".next-round");
+
+//user input
+const userInput = document.querySelector("#user-number");
+
+//generate random number 0 - 9
+
+function getNumber() {
+    let randomNum = Math.floor(Math.random() * 8 + 1);
+    return randomNum;
+}
+
+//buttons
+addButton.addEventListener("click", () => {
+    userInput.stepUp(1);
+});
+
+substrButton.addEventListener("click", () => {
+    userInput.stepDown(1);
+});
