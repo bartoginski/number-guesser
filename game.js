d174bc005e0e0d1a968f8b1ab3d22453a8ca13e3
bartoginski <ogitoziomal@gmail.com>
2020-06-19 22:39:43 +0200

    display target score

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
 // consts DOM
 
 const roundHeader = document.querySelector(".round-header");
-const targetNumber = document.querySelector(".targer-num");
+const targetNumber = document.querySelector(".target-num");
 const addButton = document.querySelector(".add");
 const substrButton = document.querySelector(".substr");
-
+const divButtons = document.querySelector(".buttons");
 //scores
 const userScore = document.querySelector(".user-score");
 const computerScore = document.querySelector(".computer-score");
@@ -16,11 +16,19 @@ const roundButton = document.querySelector(".next-round");
 //user input
 const userInput = document.querySelector("#user-number");
 
+//reset input
+userInput.value = 0;
+
 //generate random number 0 - 9
 
 function getNumber() {
     let randomNum = Math.floor(Math.random() * 8 + 1);
     return randomNum;
+}
+
+function computerChoice() {
+    let computerNum = Math.floor(Math.random() * 8 + 1);
+    return computerNum;
 }
 
 //buttons
@@ -31,3 +39,20 @@ addButton.addEventListener("click", () => {
 substrButton.addEventListener("click", () => {
     userInput.stepDown(1);
 });
+
+function makeGuess() {
+    //variables
+    let computerInput = computerChoice();
+    let userNumber = userInput.value;
+    let winNumber = getNumber();
+    console.log(computerInput, userNumber, winNumber);
+    //disable buttons
+    guessButton.disabled = true;
+    guessButton.classList.add("disable-btn");
+    addButton.classList.add("disable-btn");
+    substrButton.classList.add("disable-btn");
+    //change value header
+    targetNumber.innerText = `Target number: ${winNumber}`;
+}
+
+guessButton.addEventListener("click", makeGuess);
